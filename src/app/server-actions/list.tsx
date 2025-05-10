1b939dde45bc5531e41bba6449a235f907930fe9
Alojz Holúbek <93535633+likoli007@users.noreply.github.com>
2025-05-10 11:24:19 +0200

    feat: delete original homework files

--- src/app/server-actions/list.tsx
+++
@@ -1,7 +0,0 @@
-'use server';
-
-import { db } from '@/db';
-import { gifts } from '@/db/schema/gift';
-
-export const getGifts = async () =>
-	await db.select().from(gifts).orderBy(gifts.id).all();
