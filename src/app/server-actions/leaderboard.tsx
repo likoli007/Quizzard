c83931bd9584f36e1e54a488900fa91527bfefa0
Alojz Holúbek <93535633+likoli007@users.noreply.github.com>
2025-05-13 17:01:46 +0200

    feat: populate topic leaderboard page with real data

--- src/app/server-actions/leaderboard.tsx
+++ src/app/server-actions/leaderboard.tsx
@@ -4,6 +4,7 @@ import { sql, sum, desc, eq } from 'drizzle-orm';
 import { db } from '@/db';
 import { users } from '@/db/schema/users';
 import { quizAttempts } from '@/db/schema/quizAttempts';
+import { quizzes } from '@/db/schema/quizzes';
 
 export const getUsersByTotalPoints = async () => {
 	const rows = await db
@@ -20,6 +21,27 @@ export const getUsersByTotalPoints = async () => {
 	return rows.map(r => ({
 		id: r.id,
 		name: r.name ?? 'Anonymous',
-		score: Number(r.score ?? 0)
+		score: Number(r.score)
+	}));
+};
+
+export const getUsersByTopicPoints = async (topicId: string) => {
+	const rows = await db
+		.select({
+			id: users.id,
+			name: users.name ?? users.email,
+			score: sum(quizAttempts.score).as('totalPoints') ?? 0
+		})
+		.from(users)
+		.innerJoin(quizAttempts, eq(users.id, quizAttempts.userId))
+		.innerJoin(quizzes, eq(quizAttempts.quizId, quizzes.id))
+		.where(eq(quizzes.topicId, topicId))
+		.groupBy(users.id)
+		.orderBy(desc(sql`totalPoints`));
+
+	return rows.map(r => ({
+		id: r.id,
+		name: r.name ?? 'Anonymous',
+		score: Number(r.score)
 	}));
 };
