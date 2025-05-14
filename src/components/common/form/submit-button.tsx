18441bf3353227b97774c16eb76a769905e08b9d
Alojz Holúbek <93535633+likoli007@users.noreply.github.com>
2025-05-14 10:25:29 +0200

    fix: correct button import

--- src/components/common/form/submit-button.tsx
+++ src/components/common/form/submit-button.tsx
@@ -1,6 +1,6 @@
 import { Loader } from 'lucide-react';
 
-import { Button } from '../ui/button';
+import { Button } from '@/components/ui/button';
 
 export const SubmitButton = ({
 	isLoading = false
